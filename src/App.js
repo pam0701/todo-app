2afe4689a9c3c16689709cd3b2b8c9b96068ff04
pam0701 <psb04027@naver.com>
2022-12-05 20:57:28 +0900

    TodoContext.js 구현 및 module 설치

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 import React from "react";
-import Todos from "./todos/Todos";
+import { createGlobalStyle } from "styled-components";
+import TodoTemplate from "./components/TodoTemplate";
+import TodoHead from "./components/TodoHead";
+import TodoList from "./components/TodoList";
+import TodoCreate from "./components/TodoCreate";
+import { TodoProvider } from "./TodoContext";
 
-const App = () => {
+const GlobalStyle = createGlobalStyle`
+  body {
+    background: #e9ecef
+  }
+`;
+function App() {
   return (
-    <div>
-      <Todos />
-    </div>
+    <TodoProvider>
+      <GlobalStyle />
+      <TodoTemplate>
+        <TodoHead />
+        <TodoList />
+        <TodoCreate />
+      </TodoTemplate>
+    </TodoProvider>
   );
-};
+}
 
 export default App;
